af59db3bce254a0f90fcd711a66c7551103f4c2f
aluno <106314933+acougueirodecanela09@users.noreply.github.com>
2024-09-12 10:40:29 +0000

    slkkk

--- Java.js
+++ Java.js
@@ -1,1 +1,22 @@
+function showDialogue(character) {
+        const dialogueBox = document.getElementById('dialogue');
+            let dialogueText = '';
+
+                switch(character) {
+                        case 'ironman':
+                                    dialogueText = "Ironman: Direitos trabalhistas são fundamentais para garantir a justiça no trabalho, como o direito ao FGTS.";
+                                                break;
+                                                        case 'captain':
+                                                                    dialogueText = "Capitão América: No Brasil, a jornada máxima de trabalho é de 8 horas por dia, com direito a intervalos.";
+                                                                                break;
+                                                                                        case 'thor':
+                                                                                                    dialogueText = "Thor: Todo trabalhador tem direito a férias remuneradas e ao décimo terceiro salário. Proteger os trabalhadores é proteger Midgard!";
+                                                                                                                break;
+                                                                                                                        default:
+                                                                                                                                    dialogueText = "Clique em um Vingador para saber mais sobre direitos trabalhistas!";
+                                                                                                                                        }
+
+                                                                                                                                            dialogueBox.textContent = dialogueText;
+                                                                                                                                            }
+
 
